2bacc87ffee13e48597460c3004e10ac20e31c86
Jason Chen <jchen33@umbc.edu>
2026-01-03 15:39:39 -0500

    local database connected

--- backend/src/routes/posts.ts
+++ backend/src/routes/posts.ts
@@ -1,23 +1,29 @@
-import {Router} from 'express';
-import multer from 'multer';
+import { Router } from "express";
+import multer from "multer";
+import pool from "../db";
 
 const router = Router();
 
 // Temp storage
 const upload = multer({ dest: "uploads/" });
 
+router.post("/", upload.single("image"), async (req, res) => {
+  try {
+    const { description, latitude, longitude } = req.body;
+    const imageFile = req.file;
 
-router.post("/", upload.single("image"), (req, res) => {
-  const { description, date, latitude, longitude } = req.body;
-  const image = req.file;
+    if (!imageFile) {
+      return res.status(400).json({ error: "Image file is required" });
+    }
 
-  console.log("📍 Location:", latitude, longitude);
-  console.log("📝 Description:", description);
-  console.log("📸 Image:", image?.filename);
-
-  res.status(201).json({
-    message: "Post received suåccessfully",
-  });
+    await pool.query(
+      `INSERT INTO posts (image_url, description, latitude, longitude) VALUES ($1, $2, $3, $4)`,
+      [imageFile.filename, description, latitude, longitude]
+    );
+    res.status(201).json({ message: "Post created successfully" });
+  } catch (error) {
+    console.error("Error handling post request:", error);
+  }
 });
 
 export default router;
