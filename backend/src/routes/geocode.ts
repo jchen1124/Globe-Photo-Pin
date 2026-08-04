48d07b2de87ba53afc7cb5ce65c5fc6e01132322
Jason Chen <jchen33@umbc.edu>
2026-08-04 17:39:28 -0400

    initial full redis cache implementation for reverse geocode results

--- backend/src/routes/geocode.ts
+++ backend/src/routes/geocode.ts
@@ -1,35 +1,55 @@
-import axios from 'axios';
-import {Router} from 'express';
-import dotenv from 'dotenv';    
+import axios from "axios";
+import { Router } from "express";
+import dotenv from "dotenv";
+import { redisClient } from "../redis";
 
 dotenv.config();
 
 const router = Router();
 
 router.get("/", async (req, res) => {
-    // console.log("Geocode route called");
-    const { latitude, longitude } = req.query;
+  // console.log("Geocode route called");
+  const { latitude, longitude } = req.query;
 
-    if (!latitude || !longitude) {
-        return res.status(400).json({ error: "Latitude and longitude are required" });
+  if (!latitude || !longitude) {
+    return res
+      .status(400)
+      .json({ error: "Latitude and longitude are required" });
+  }
+  const lat = Number(latitude);
+  const lng = Number(longitude);
+
+  if (!Number.isFinite(lat) || !Number.isFinite(lng)) {
+    return res
+      .status(400)
+      .json({ error: "Latitude and longitude must be valid numbers" });
+  }
+
+  const cacheKey = `geocode:v1:${lat.toFixed(5)}:${lng.toFixed(5)}`;
+  try {
+    // const response = await fetch(url);
+    // const data = await response.json();
+    const cachedData = await redisClient.get(cacheKey);
+    if (cachedData) {
+      console.log("Cache hit for geocode data", cachedData);
+      return res.json(JSON.parse(cachedData));
     }
 
+    console.log("Cache miss for geocode data, fetching from Mapbox API");
     const MAPBOX_TOKEN = process.env.VITE_MAPBOX_TOKEN;
-    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${MAPBOX_TOKEN}`;
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=${MAPBOX_TOKEN}`;
 
-    try{
-        // const response = await fetch(url);
-        // const data = await response.json();
-
-        const response = await axios.get(url);
-        const place = response.data.features[0]?.place_name || "Unknown location";
-        console.log("Geocode response:", place);
-        res.json({ address: place });
-        
-    }catch(error){
-        console.error("Error fetching geocode data:", error);
-        res.status(500).json({ error: "Internal server error" });
-    }
-})
+    const response = await axios.get(url);
+    const place = response.data.features[0]?.place_name || "Unknown location";
+    console.log("Geocode response:", place);
+    await redisClient.set(cacheKey, JSON.stringify({ address: place }), {
+      EX: 60 * 5,
+    });
+    return res.json({ address: place });
+  } catch (error) {
+    console.error("Error fetching geocode data:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 export default router;
